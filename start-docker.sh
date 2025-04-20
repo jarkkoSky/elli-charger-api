ea4de8c18804dbb20c09368081e282e178c441b0
Jarkko Skyttälä <jarkko92@gmail.com>
2025-04-20 18:30:50 +0300

    cleanup start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 CONTAINER_NAME=elli-charger-api-container
+IMAGE_NAME=elli-charger-api
 
 source .env
 
 docker rm -f $CONTAINER_NAME || true
 
-docker build -t elli-charger-api .
-docker run --name $CONTAINER_NAME --restart unless-stopped -d --env-file .env -p ${SERVER_PORT}:${SERVER_PORT} elli-charger-api
+docker build -t $IMAGE_NAME .
+docker run --name $CONTAINER_NAME --restart unless-stopped -d --env-file .env -p ${SERVER_PORT}:${SERVER_PORT} $IMAGE_NAME
