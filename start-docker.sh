8cf339cc6400a6af625c16b39e319ceed91d7e30
Jarkko Skyttälä <jarkko92@gmail.com>
2025-04-20 18:22:24 +0300

    docker command to restart container unless stopped

--- start-docker.sh
+++ start-docker.sh
@@ -3,4 +3,4 @@
 source .env
 
 docker build -t elli-charger-api .
-docker run -d --env-file .env -p ${SERVER_PORT}:${SERVER_PORT} elli-charger-api
+docker run --restart unless-stopped -d --env-file .env -p ${SERVER_PORT}:${SERVER_PORT} elli-charger-api
