a0b646d4ba460b612d0c322d9cd2e891dd115f33
Jarkko Skyttälä <jarkko92@gmail.com>
2025-04-20 18:27:40 +0300

    should delete existing container

--- start-docker.sh
+++ start-docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+CONTAINER_NAME=elli-charger-api-container
+
 source .env
 
+docker rm -f $CONTAINER_NAME || true
+
 docker build -t elli-charger-api .
-docker run --restart unless-stopped -d --env-file .env -p ${SERVER_PORT}:${SERVER_PORT} elli-charger-api
+docker run --name $CONTAINER_NAME --restart unless-stopped -d --env-file .env -p ${SERVER_PORT}:${SERVER_PORT} elli-charger-api
